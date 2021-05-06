712a1f5b667ffea239ace5415bc5b3a68329a5e1
Ruben Taelman <ruben.taelman@ugent.be>
2021-05-06 11:40:11 +0200

    Expose Templates.QUERIES_DIRECTORY

--- lib/Templates.ts
+++ lib/Templates.ts
@@ -9,4 +9,5 @@ export const Templates = {
   ENHANCEMENT_FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-auxiliary-config-subject.json'),
   QUERY_CONFIG: Path.join(__dirname, '../templates/query-config.json'),
   SERVER_CONFIG: Path.join(__dirname, '../templates/server-config.json'),
+  QUERIES_DIRECTORY: Path.join(__dirname, '../templates/queries'),
 };
