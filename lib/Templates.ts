71c0e1d179d66ff9f1d4e7101a47d71d35af9a71
Ruben Taelman <ruben.taelman@ugent.be>
2022-05-09 14:35:36 +0200

    Introduce more variance in Solid pod fragmentation

--- lib/Templates.ts
+++ lib/Templates.ts
@@ -5,7 +5,7 @@ import Path from 'path';
  */
 export const Templates = {
   ENHANCEMENT_CONFIG: Path.join(__dirname, '../templates/enhancer-config-dummy.json'),
-  FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-config-subject.json'),
+  FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-config-pod.json'),
   ENHANCEMENT_FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-auxiliary-config-subject.json'),
   QUERY_CONFIG: Path.join(__dirname, '../templates/query-config.json'),
   SERVER_CONFIG: Path.join(__dirname, '../templates/server-config.json'),
