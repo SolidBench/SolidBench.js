97fd4b49e8dde26270ac05c21ec84ece005204a8
Sindhu-Vasireddy <sindhu.vasireddy@ugent.be>
2023-01-30 15:14:45 +0100

    Updated LDBC SNB Enhancer version

--- lib/Templates.ts
+++ lib/Templates.ts
@@ -4,7 +4,7 @@ import Path from 'path';
  * Paths to default configuration files.
  */
 export const Templates = {
-  ENHANCEMENT_CONFIG: Path.join(__dirname, '../templates/enhancer-config-dummy.json'),
+  ENHANCEMENT_CONFIG: Path.join(__dirname, '../templates/enhancer-config-pod.json'),
   FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-config-pod.json'),
   ENHANCEMENT_FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-auxiliary-config-pod.json'),
   QUERY_CONFIG: Path.join(__dirname, '../templates/query-config.json'),
