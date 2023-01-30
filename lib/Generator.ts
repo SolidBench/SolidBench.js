43a170e725554df753a6870cb4f1b01eab83ec3e
Sindhu-Vasireddy <sindhu.vasireddy@ugent.be>
2023-01-30 16:37:57 +0100

    Updated LDBC SNB Enhancer version

--- lib/Generator.ts
+++ lib/Generator.ts
@@ -97,8 +97,7 @@ export class Generator {
     // Create params.ini file
     const paramsTemplate = await fs.promises.readFile(Path.join(__dirname, '../templates/params.ini'), 'utf8');
     const paramsPath = Path.join(this.cwd, 'params.ini');
-    // eslint-disable-next-line unicorn/prefer-string-replace-all
-    await fs.promises.writeFile(paramsPath, paramsTemplate.replace(/SCALE/ug, this.scale), 'utf8');
+    await fs.promises.writeFile(paramsPath, paramsTemplate.replaceAll(/SCALE/ug, this.scale), 'utf8');
 
     // Pull the base Docker image
     const dockerode = new Dockerode();
