6a938274c6be356281be2604cadd977175520f50
surilindur <surilindur@users.noreply.github.com>
2024-04-30 16:00:11 +0200

    Fix linter errors in Templates.ts

--- lib/Templates.ts
+++ lib/Templates.ts
@@ -1,15 +1,15 @@
-import Path from 'path';
+import { join } from 'node:path';
 
 /**
  * Paths to default configuration files.
  */
 export const Templates = {
-  ENHANCEMENT_CONFIG: Path.join(__dirname, '../templates/enhancer-config-pod.json'),
-  FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-config-pod.json'),
-  ENHANCEMENT_FRAGMENT_CONFIG: Path.join(__dirname, '../templates/fragmenter-auxiliary-config-pod.json'),
-  QUERY_CONFIG: Path.join(__dirname, '../templates/query-config.json'),
-  SERVER_CONFIG: Path.join(__dirname, '../templates/server-config.json'),
-  QUERIES_DIRECTORY: Path.join(__dirname, '../templates/queries'),
-  VALIDATION_CONFIG: Path.join(__dirname, '../templates/validation-config.json'),
+  ENHANCEMENT_CONFIG: join(__dirname, '../templates/enhancer-config-pod.json'),
+  FRAGMENT_CONFIG: join(__dirname, '../templates/fragmenter-config-pod.json'),
+  ENHANCEMENT_FRAGMENT_CONFIG: join(__dirname, '../templates/fragmenter-auxiliary-config-pod.json'),
+  QUERY_CONFIG: join(__dirname, '../templates/query-config.json'),
+  SERVER_CONFIG: join(__dirname, '../templates/server-config.json'),
+  QUERIES_DIRECTORY: join(__dirname, '../templates/queries'),
+  VALIDATION_CONFIG: join(__dirname, '../templates/validation-config.json'),
   VALIDATION_PARAMS_URL: 'https://cloud.ilabt.imec.be/index.php/s/bBZZKb7cP95WgcD/download/validation_params.zip',
 };
