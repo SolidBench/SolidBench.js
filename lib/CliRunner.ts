deb504b90cf397a31f6f6f05a24a9ff522757283
surilindur <surilindur@users.noreply.github.com>
2024-04-30 15:38:57 +0200

    Bump yargs to v17

--- lib/CliRunner.ts
+++ lib/CliRunner.ts
@@ -2,7 +2,7 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 export function runCli(cwd: string, argv: string[]): void {
-  const { argv: params } = yargs(hideBin(argv))
+  yargs(hideBin(argv))
     .options({
       cwd: { type: 'string', default: cwd, describe: 'The current working directory', defaultDescription: '.' },
       verbose: { type: 'boolean', describe: 'If more output should be printed' },
