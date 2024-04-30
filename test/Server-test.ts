d5999723c32453ad959a8b4fe423297a0360412c
surilindur <surilindur@users.noreply.github.com>
2024-04-30 16:12:12 +0200

    Add new test case to cover remaining line

--- test/Server-test.ts
+++ test/Server-test.ts
@@ -11,33 +11,57 @@ jest.mock<typeof import('@solid/community-server')>('@solid/community-server', (
 
 describe('Server', () => {
   let server: Server;
+  let baseUrl: string;
+
+  const configPath = 'CONFIG';
+  const port = 3_000;
+  const logLevel = 'info';
+  const rootFilePath = 'out-fragments/http/localhost_3000/';
 
   beforeEach(() => {
-    server = new Server({
-      configPath: 'CONFIG',
-      port: 3_000,
-      logLevel: 'info',
-      baseUrl: undefined,
-      rootFilePath: 'out-fragments/http/localhost_3000/',
-    });
     run.mockReset();
   });
 
-  it('serve', async() => {
+  it('calls serve', async() => {
+    server = new Server({ configPath, port, logLevel, baseUrl, rootFilePath });
     await server.serve();
     expect(run).toHaveBeenCalledWith(
       {
         loaderProperties: {
           mainModulePath: join(__dirname, '..'),
           typeChecking: false,
-          logLevel: 'info',
+          logLevel,
         },
-        config: 'CONFIG',
+        config: configPath,
         variableBindings: {
           'urn:solid-server:default:variable:baseUrl': 'http://localhost:3000/',
-          'urn:solid-server:default:variable:loggingLevel': 'info',
-          'urn:solid-server:default:variable:port': 3_000,
-          'urn:solid-server:default:variable:rootFilePath': 'out-fragments/http/localhost_3000/',
+          'urn:solid-server:default:variable:loggingLevel': logLevel,
+          'urn:solid-server:default:variable:port': port,
+          'urn:solid-server:default:variable:rootFilePath': rootFilePath,
+          'urn:solid-server:default:variable:seededPodConfigJson': '',
+          'urn:solid-server:default:variable:showStackTrace': false,
+        },
+      },
+    );
+  });
+
+  it('calls serve with baseUrl defined', async() => {
+    baseUrl = 'http://localhost:1234/';
+    server = new Server({ configPath, port, logLevel, baseUrl, rootFilePath });
+    await server.serve();
+    expect(run).toHaveBeenCalledWith(
+      {
+        loaderProperties: {
+          mainModulePath: join(__dirname, '..'),
+          typeChecking: false,
+          logLevel,
+        },
+        config: configPath,
+        variableBindings: {
+          'urn:solid-server:default:variable:baseUrl': baseUrl,
+          'urn:solid-server:default:variable:loggingLevel': logLevel,
+          'urn:solid-server:default:variable:port': port,
+          'urn:solid-server:default:variable:rootFilePath': rootFilePath,
           'urn:solid-server:default:variable:seededPodConfigJson': '',
           'urn:solid-server:default:variable:showStackTrace': false,
         },
