a5c0657886d2bd4861b60fe3ba665ce791a941cf
surilindur <surilindur@users.noreply.github.com>
2024-05-02 15:20:58 +0200

    Additional fixes to linter errors

--- lib/CliRunner.ts
+++ lib/CliRunner.ts
@@ -2,7 +2,7 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 export function runCli(cwd: string, argv: string[]): void {
-  yargs(hideBin(argv))
+  const _ = yargs(hideBin(argv))
     .options({
       cwd: { type: 'string', default: cwd, describe: 'The current working directory', defaultDescription: '.' },
       verbose: { type: 'boolean', describe: 'If more output should be printed' },
