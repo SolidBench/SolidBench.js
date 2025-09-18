3f2a90fe178ef4c71edd60edd08c3a1d82f1a770
RubenEschauzier <ruben.eschauzier@gmail.com>
2025-09-18 13:07:53 +0200

    Fix linter issue

--- lib/Generator.ts
+++ lib/Generator.ts
@@ -239,11 +239,12 @@ export class Generator {
   }
 
   protected async generateVariables(): Promise<Record<string, string>> {
-    const templateMappings = (await readdir(join(__dirname, '../templates/queries/')))
+    const templateMappings: string[][] = (await readdir(join(__dirname, '../templates/queries/')))
       .map(name => [ `urn:variables:query-templates:${name}`, join(__dirname, `../templates/queries/${name}`) ]);
-    const refinementMappings = (await readdir(join(__dirname, '../templates/refinements/')))
-      .map(name => [ `urn:variables:query-refinements:${name}`, join(__dirname, `../templates/refinements/${name}`) ])
-    return Object.fromEntries([...templateMappings, ...refinementMappings]);
+    const refinementMappings: string[][] = (await readdir(join(__dirname, '../templates/refinements/')))
+      .map(name => [ `urn:variables:query-refinements:${name}`, join(__dirname, `../templates/refinements/${name}`) ]);
+    const result = <Record<string, string>> Object.fromEntries([ ...templateMappings, ...refinementMappings ]);
+    return result;
   }
 
   /**
